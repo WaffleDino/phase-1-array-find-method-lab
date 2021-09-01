f35f8ecc2b7122b809ad6e0faf9b905cdca63c8e
Jesse <jdnely@gmail.com>
2021-09-01 10:14:47 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 
+// function superbowlWin(record){
+//     record.find(season(season)(result = "W")
+// }
+
+// let superbowlVictories = (record) => { record.find="w";
+// return superbowlVictories
+
+// results= function(element) {
+//     return (element.result==="W") 
+// }
+
+// function superbowlWin(array) {
+//     const wins= array.find(results)
+//     if (wins==="undefined") {
+//         return ("undefined");
+//     } else {
+//     return wins.year;
+//     }
+// }
+function superbowlWin(array1) {
+    let winningYear = array1.find((array1) => array1.result === "W");
+    if (!!winningYear) {
+        return winningYear.year;    
+    } 
+}
+  
